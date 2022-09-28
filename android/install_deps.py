5817693e609821f3addae2946963da6a36aa399b
melpon <shigemasa7watanabe+github@gmail.com>
2022-09-29 00:06:52 +0900

    ビルドプロセスの中に依存ライブラリのダウンロードを追加する

---
+++ android/install_deps.py
@@ -0,0 +1,465 @@
+import subprocess
+import logging
+import os
+import urllib.parse
+import zipfile
+import tarfile
+import shutil
+import platform
+import argparse
+from typing import Callable, Optional, List, Union, Dict
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class ChangeDirectory(object):
+    def __init__(self, cwd):
+        self._cwd = cwd
+
+    def __enter__(self):
+        self._old_cwd = os.getcwd()
+        logging.debug(f'pushd {self._old_cwd} --> {self._cwd}')
+        os.chdir(self._cwd)
+
+    def __exit__(self, exctype, excvalue, trace):
+        logging.debug(f'popd {self._old_cwd} <-- {self._cwd}')
+        os.chdir(self._old_cwd)
+        return False
+
+
+def cd(cwd):
+    return ChangeDirectory(cwd)
+
+
+def cmd(args, **kwargs):
+    logging.debug(f'+{args} {kwargs}')
+    if 'check' not in kwargs:
+        kwargs['check'] = True
+    if 'resolve' in kwargs:
+        resolve = kwargs['resolve']
+        del kwargs['resolve']
+    else:
+        resolve = True
+    if resolve:
+        args = [shutil.which(args[0]), *args[1:]]
+    return subprocess.run(args, **kwargs)
+
+
+# 標準出力をキャプチャするコマンド実行。シェルの `cmd ...` や $(cmd ...) と同じ
+def cmdcap(args, **kwargs):
+    # 3.7 でしか使えない
+    # kwargs['capture_output'] = True
+    kwargs['stdout'] = subprocess.PIPE
+    kwargs['stderr'] = subprocess.PIPE
+    kwargs['encoding'] = 'utf-8'
+    return cmd(args, **kwargs).stdout.strip()
+
+
+def rm_rf(path: str):
+    if not os.path.exists(path):
+        logging.debug(f'rm -rf {path} => path not found')
+        return
+    if os.path.isfile(path) or os.path.islink(path):
+        os.remove(path)
+        logging.debug(f'rm -rf {path} => file removed')
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        logging.debug(f'rm -rf {path} => directory removed')
+
+
+def mkdir_p(path: str):
+    if os.path.exists(path):
+        logging.debug(f'mkdir -p {path} => already exists')
+        return
+    os.makedirs(path, exist_ok=True)
+    logging.debug(f'mkdir -p {path} => directory created')
+
+
+if platform.system() == 'Windows':
+    PATH_SEPARATOR = ';'
+else:
+    PATH_SEPARATOR = ':'
+
+
+def add_path(path: str, is_after=False):
+    logging.debug(f'add_path: {path}')
+    if 'PATH' not in os.environ:
+        os.environ['PATH'] = path
+        return
+
+    if is_after:
+        os.environ['PATH'] = os.environ['PATH'] + PATH_SEPARATOR + path
+    else:
+        os.environ['PATH'] = path + PATH_SEPARATOR + os.environ['PATH']
+
+
+def download(url: str, output_dir: Optional[str] = None, filename: Optional[str] = None) -> str:
+    if filename is None:
+        output_path = urllib.parse.urlparse(url).path.split('/')[-1]
+    else:
+        output_path = filename
+
+    if output_dir is not None:
+        output_path = os.path.join(output_dir, output_path)
+
+    if os.path.exists(output_path):
+        return output_path
+
+    try:
+        if shutil.which('curl') is not None:
+            cmd(["curl", "-fLo", output_path, url])
+        else:
+            cmd(["wget", "-cO", output_path, url])
+    except Exception:
+        # ゴミを残さないようにする
+        if os.path.exists(output_path):
+            os.remove(output_path)
+        raise
+
+    return output_path
+
+
+def read_version_file(path: str) -> Dict[str, str]:
+    versions = {}
+
+    lines = open(path).readlines()
+    for line in lines:
+        line = line.strip()
+
+        # コメント行
+        if line[:1] == '#':
+            continue
+
+        # 空行
+        if len(line) == 0:
+            continue
+
+        [a, b] = map(lambda x: x.strip(), line.split('=', 2))
+        versions[a] = b.strip('"')
+
+    return versions
+
+
+# dir 以下にある全てのファイルパスを、dir2 からの相対パスで返す
+def enum_all_files(dir, dir2):
+    for root, _, files in os.walk(dir):
+        for file in files:
+            yield os.path.relpath(os.path.join(root, file), dir2)
+
+
+def versioned(func):
+    def wrapper(version, version_file, *args, **kwargs):
+        if 'ignore_version' in kwargs:
+            if kwargs.get('ignore_version'):
+                rm_rf(version_file)
+            del kwargs['ignore_version']
+
+        if os.path.exists(version_file):
+            ver = open(version_file).read()
+            if ver.strip() == version.strip():
+                return
+
+        r = func(version=version, *args, **kwargs)
+
+        with open(version_file, 'w') as f:
+            f.write(version)
+
+        return r
+
+    return wrapper
+
+
+# アーカイブが単一のディレクトリに全て格納されているかどうかを調べる。
+#
+# 単一のディレクトリに格納されている場合はそのディレクトリ名を返す。
+# そうでない場合は None を返す。
+def _is_single_dir(infos: List[Union[zipfile.ZipInfo, tarfile.TarInfo]],
+                   get_name: Callable[[Union[zipfile.ZipInfo, tarfile.TarInfo]], str],
+                   is_dir: Callable[[Union[zipfile.ZipInfo, tarfile.TarInfo]], bool]) -> Optional[str]:
+    # tarfile: ['path', 'path/to', 'path/to/file.txt']
+    # zipfile: ['path/', 'path/to/', 'path/to/file.txt']
+    # どちらも / 区切りだが、ディレクトリの場合、後ろに / が付くかどうかが違う
+    dirname = None
+    for info in infos:
+        name = get_name(info)
+        n = name.rstrip('/').find('/')
+        if n == -1:
+            # ルートディレクトリにファイルが存在している
+            if not is_dir(info):
+                return None
+            dir = name.rstrip('/')
+        else:
+            dir = name[0:n]
+        # ルートディレクトリに２個以上のディレクトリが存在している
+        if dirname is not None and dirname != dir:
+            return None
+        dirname = dir
+
+    return dirname
+
+
+def is_single_dir_tar(tar: tarfile.TarFile) -> Optional[str]:
+    return _is_single_dir(tar.getmembers(), lambda t: t.name, lambda t: t.isdir())
+
+
+def is_single_dir_zip(zip: zipfile.ZipFile) -> Optional[str]:
+    return _is_single_dir(zip.infolist(), lambda z: z.filename, lambda z: z.is_dir())
+
+
+# 解凍した上でファイル属性を付与する
+def _extractzip(z: zipfile.ZipFile, path: str):
+    z.extractall(path)
+    if platform.system() == 'Windows':
+        return
+    for info in z.infolist():
+        if info.is_dir():
+            continue
+        filepath = os.path.join(path, info.filename)
+        mod = info.external_attr >> 16
+        if (mod & 0o120000) == 0o120000:
+            # シンボリックリンク
+            with open(filepath, 'r') as f:
+                src = f.read()
+            os.remove(filepath)
+            with cd(os.path.dirname(filepath)):
+                if os.path.exists(src):
+                    os.symlink(src, filepath)
+        if os.path.exists(filepath):
+            # 普通のファイル
+            os.chmod(filepath, mod & 0o777)
+
+
+# zip または tar.gz ファイルを展開する。
+#
+# 展開先のディレクトリは {output_dir}/{output_dirname} となり、
+# 展開先のディレクトリが既に存在していた場合は削除される。
+#
+# もしアーカイブの内容が単一のディレクトリであった場合、
+# そのディレクトリは無いものとして展開される。
+#
+# つまりアーカイブ libsora-1.23.tar.gz の内容が
+# ['libsora-1.23', 'libsora-1.23/file1', 'libsora-1.23/file2']
+# であった場合、extract('libsora-1.23.tar.gz', 'out', 'libsora') のようにすると
+# - out/libsora/file1
+# - out/libsora/file2
+# が出力される。
+#
+# また、アーカイブ libsora-1.23.tar.gz の内容が
+# ['libsora-1.23', 'libsora-1.23/file1', 'libsora-1.23/file2', 'LICENSE']
+# であった場合、extract('libsora-1.23.tar.gz', 'out', 'libsora') のようにすると
+# - out/libsora/libsora-1.23/file1
+# - out/libsora/libsora-1.23/file2
+# - out/libsora/LICENSE
+# が出力される。
+def extract(file: str, output_dir: str, output_dirname: str, filetype: Optional[str] = None):
+    path = os.path.join(output_dir, output_dirname)
+    logging.info(f"Extract {file} to {path}")
+    if filetype == 'gzip' or file.endswith('.tar.gz'):
+        rm_rf(path)
+        with tarfile.open(file) as t:
+            dir = is_single_dir_tar(t)
+            if dir is None:
+                os.makedirs(path, exist_ok=True)
+                t.extractall(path)
+            else:
+                logging.info(f"Directory {dir} is stripped")
+                path2 = os.path.join(output_dir, dir)
+                rm_rf(path2)
+                t.extractall(output_dir)
+                if path != path2:
+                    logging.debug(f"mv {path2} {path}")
+                    os.replace(path2, path)
+    elif filetype == 'zip' or file.endswith('.zip'):
+        rm_rf(path)
+        with zipfile.ZipFile(file) as z:
+            dir = is_single_dir_zip(z)
+            if dir is None:
+                os.makedirs(path, exist_ok=True)
+                # z.extractall(path)
+                _extractzip(z, path)
+            else:
+                logging.info(f"Directory {dir} is stripped")
+                path2 = os.path.join(output_dir, dir)
+                rm_rf(path2)
+                # z.extractall(output_dir)
+                _extractzip(z, output_dir)
+                if path != path2:
+                    logging.debug(f"mv {path2} {path}")
+                    os.replace(path2, path)
+    else:
+        raise Exception('file should end with .tar.gz or .zip')
+
+
+def clone_and_checkout(url, version, dir, fetch, fetch_force):
+    if fetch_force:
+        rm_rf(dir)
+
+    if not os.path.exists(os.path.join(dir, '.git')):
+        cmd(['git', 'clone', url, dir])
+        fetch = True
+
+    if fetch:
+        with cd(dir):
+            cmd(['git', 'fetch'])
+            cmd(['git', 'reset', '--hard'])
+            cmd(['git', 'clean', '-df'])
+            cmd(['git', 'checkout', '-f', version])
+
+
+def git_clone_shallow(url, hash, dir):
+    rm_rf(dir)
+    mkdir_p(dir)
+    with cd(dir):
+        cmd(['git', 'init'])
+        cmd(['git', 'remote', 'add', 'origin', url])
+        cmd(['git', 'fetch', '--depth=1', 'origin', hash])
+        cmd(['git', 'reset', '--hard', 'FETCH_HEAD'])
+
+
+@versioned
+def install_webrtc(version, source_dir, install_dir, platform: str):
+    win = platform.startswith("windows_")
+    filename = f'webrtc.{platform}.{"zip" if win else "tar.gz"}'
+    rm_rf(os.path.join(source_dir, filename))
+    archive = download(
+        f'https://github.com/shiguredo-webrtc-build/webrtc-build/releases/download/{version}/{filename}',
+        output_dir=source_dir)
+    rm_rf(os.path.join(install_dir, 'webrtc'))
+    extract(archive, output_dir=install_dir, output_dirname='webrtc')
+    # Dart はロングパスをサポートしていない (https://github.com/dart-lang/sdk/issues/27825) ため、
+    # ディレクトリが奥深くまであるディレクトリは削除する。
+    rm_rf(os.path.join(install_dir, 'webrtc', 'include', 'examples'))
+    rm_rf(os.path.join(install_dir, 'webrtc', 'include', 'buildtools'))
+
+
+@versioned
+def install_llvm(version, install_dir,
+                 tools_url, tools_commit,
+                 libcxx_url, libcxx_commit,
+                 buildtools_url, buildtools_commit):
+    llvm_dir = os.path.join(install_dir, 'llvm')
+    rm_rf(llvm_dir)
+    mkdir_p(llvm_dir)
+    with cd(llvm_dir):
+        # tools の update.py を叩いて特定バージョンの clang バイナリを拾う
+        git_clone_shallow(tools_url, tools_commit, 'tools')
+        with cd('tools'):
+            cmd(['python3',
+                os.path.join('clang', 'scripts', 'update.py'),
+                '--output-dir', os.path.join(llvm_dir, 'clang')])
+
+        # 特定バージョンの libcxx を利用する
+        git_clone_shallow(libcxx_url, libcxx_commit, 'libcxx')
+
+        # __config_site のために特定バージョンの buildtools を取得する
+        git_clone_shallow(buildtools_url, buildtools_commit, 'buildtools')
+        with cd('buildtools'):
+            cmd(['git', 'reset', '--hard', buildtools_commit])
+        shutil.copyfile(os.path.join(llvm_dir, 'buildtools', 'third_party', 'libc++', '__config_site'),
+                        os.path.join(llvm_dir, 'libcxx', 'include', '__config_site'))
+
+
+@versioned
+def install_boost(version, source_dir, install_dir, sora_version, platform: str):
+    win = platform.startswith("windows_")
+    filename = f'boost-{version}_sora-cpp-sdk-{sora_version}_{platform}.{"zip" if win else "tar.gz"}'
+    rm_rf(os.path.join(source_dir, filename))
+    archive = download(
+        f'https://github.com/shiguredo/sora-cpp-sdk/releases/download/{sora_version}/{filename}',
+        output_dir=source_dir)
+    rm_rf(os.path.join(install_dir, 'boost'))
+    extract(archive, output_dir=install_dir, output_dirname='boost')
+
+
+@versioned
+def install_sora(version, source_dir, install_dir, platform: str):
+    win = platform.startswith("windows_")
+    filename = f'sora-cpp-sdk-{version}_{platform}.{"zip" if win else "tar.gz"}'
+    rm_rf(os.path.join(source_dir, filename))
+    archive = download(
+        f'https://github.com/shiguredo/sora-cpp-sdk/releases/download/{version}/{filename}',
+        output_dir=source_dir)
+    rm_rf(os.path.join(install_dir, 'sora'))
+    extract(archive, output_dir=install_dir, output_dirname='sora')
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+AVAILABLE_TARGETS = ['android']
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", choices=AVAILABLE_TARGETS)
+    parser.add_argument("--webrtc-version", required=True)
+    parser.add_argument("--boost-version", required=True)
+    parser.add_argument("--sora-version", required=True)
+
+    args = parser.parse_args()
+    source_dir = os.path.join(BASE_DIR, '_install')
+    install_dir = os.path.join(BASE_DIR, '_install')
+    mkdir_p(source_dir)
+    mkdir_p(install_dir)
+
+    with cd(BASE_DIR):
+        install_webrtc_args = {
+            'version': args.webrtc_version,
+            'version_file': os.path.join(install_dir, 'webrtc.version'),
+            'source_dir': source_dir,
+            'install_dir': install_dir,
+            'platform': args.target,
+        }
+
+        install_webrtc(**install_webrtc_args)
+
+        webrtc_version = read_version_file(os.path.join(install_dir, 'webrtc', 'VERSIONS'))
+
+        # LLVM
+        tools_url = webrtc_version['WEBRTC_SRC_TOOLS_URL']
+        tools_commit = webrtc_version['WEBRTC_SRC_TOOLS_COMMIT']
+        libcxx_url = webrtc_version['WEBRTC_SRC_BUILDTOOLS_THIRD_PARTY_LIBCXX_TRUNK_URL']
+        libcxx_commit = webrtc_version['WEBRTC_SRC_BUILDTOOLS_THIRD_PARTY_LIBCXX_TRUNK_COMMIT']
+        buildtools_url = webrtc_version['WEBRTC_SRC_BUILDTOOLS_URL']
+        buildtools_commit = webrtc_version['WEBRTC_SRC_BUILDTOOLS_COMMIT']
+        install_llvm_args = {
+            'version':
+                f'{tools_url}.{tools_commit}.'
+                f'{libcxx_url}.{libcxx_commit}.'
+                f'{buildtools_url}.{buildtools_commit}',
+            'version_file': os.path.join(install_dir, 'llvm.version'),
+            'install_dir': install_dir,
+            'tools_url': tools_url,
+            'tools_commit': tools_commit,
+            'libcxx_url': libcxx_url,
+            'libcxx_commit': libcxx_commit,
+            'buildtools_url': buildtools_url,
+            'buildtools_commit': buildtools_commit,
+        }
+        install_llvm(**install_llvm_args)
+
+        # Boost
+        install_boost_args = {
+            'version': args.boost_version,
+            'version_file': os.path.join(install_dir, 'boost.version'),
+            'source_dir': source_dir,
+            'install_dir': install_dir,
+            'sora_version': args.sora_version,
+            'platform': args.target,
+        }
+        install_boost(**install_boost_args)
+
+        # Sora C++ SDK
+        install_sora_args = {
+            'version': args.sora_version,
+            'version_file': os.path.join(install_dir, 'sora.version'),
+            'source_dir': source_dir,
+            'install_dir': install_dir,
+            'platform': args.target,
+        }
+        install_sora(**install_sora_args)
+
+
+if __name__ == '__main__':
+    main()
