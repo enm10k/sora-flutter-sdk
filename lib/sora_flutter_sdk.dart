5a80fb16b5ed684e96ef07df0251278d9628c529
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2022-10-05 16:00:53 +0900

    描画用ウィジェットを実装

--- lib/sora_flutter_sdk.dart
+++ lib/sora_flutter_sdk.dart
@@ -1,3 +1,4 @@
 export 'src/client.dart'
     show SoraClient, SoraClientConfig, SoraRole, SoraVideoCodecType;
 export 'src/video_track.dart' show SoraVideoTrack;
+export 'src/renderer.dart' show SoraRenderer;
