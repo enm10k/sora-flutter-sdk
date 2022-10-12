1e8b2804bcc9667d98adaa721e21fb26182141be
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2022-10-12 17:11:00 +0900

    サンプルに接続と切断のボタンを用意する

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   SoraClient? _soraClient;
+  var _isConnected = false;
 
   @override
   void initState() {
@@ -32,25 +33,6 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) {
       return;
     }
-
-    final config = SoraClientConfig(
-      signalingUrls:
-          Environment.urlCandidates.map((e) => e.toString()).toList(),
-      channelId: Environment.channelId,
-      role: SoraRole.sendrecv,
-    );
-    final soraClient = await SoraClient.create(config)
-      ..onAddTrack = (SoraVideoTrack track) {
-        setState(() {/* soraClient.tracks の数が変動したので描画し直す */});
-      }
-      ..onRemoveTrack = (SoraVideoTrack track) {
-        setState(() {/* soraClient.tracks の数が変動したので描画し直す */});
-      };
-    await soraClient.connect();
-
-    setState(() {
-      _soraClient = soraClient;
-    });
   }
 
   @override
@@ -61,22 +43,89 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: GridView.count(
-            crossAxisCount: 2,
-            children: _soraClient == null
-                ? []
-                : _soraClient!.tracks
-                    .map(
-                      (track) => SoraRenderer(
-                        width: 320,
-                        height: 240,
-                        track: track,
-                      ),
-                    )
-                    .toList(),
+          child: Column(
+            children: [
+              Center(
+                child: _buildRenderers(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      await _connect();
+                    },
+                    child: const Text('接続する'),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await _disconnect();
+                    },
+                    child: const Text('切断する'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildRenderers() {
+    var renderers = List<SoraRenderer>.empty();
+    if (_soraClient != null) {
+      renderers = _soraClient!.tracks
+          .map((track) => SoraRenderer(
+                width: 320,
+                height: 240,
+                track: track,
+              ))
+          .toList();
+    }
+
+    return GridView.count(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      children: renderers,
+    );
+  }
+
+  Future<void> _connect() async {
+    if (_soraClient != null) {
+      dispose();
+    }
+
+    final config = SoraClientConfig(
+      signalingUrls:
+          Environment.urlCandidates.map((e) => e.toString()).toList(),
+      channelId: Environment.channelId,
+      role: SoraRole.sendrecv,
+    );
+
+    final soraClient = await SoraClient.create(config)
+      ..onAddTrack = (SoraVideoTrack track) {
+        setState(() {/* soraClient.tracks の数が変動したので描画し直す */});
+      }
+      ..onRemoveTrack = (SoraVideoTrack track) {
+        setState(() {/* soraClient.tracks の数が変動したので描画し直す */});
+      };
+
+    await soraClient.connect();
+
+    setState(() {
+      _soraClient = soraClient;
+      _isConnected = true;
+    });
+  }
+
+  Future<void> _disconnect() async {
+    await _soraClient?.dispose();
+    setState(() {
+      _soraClient = null;
+      _isConnected = false;
+    });
   }
 }
