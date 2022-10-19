06aff6f1f0b43d256b70d4c54802ad4fdcbfccdd
miosakuma <mio.sakuma.gml@gmail.com>
2022-10-19 19:00:33 +0900

    SoraDataChannel  を参照可能にする

--- lib/sora_flutter_sdk.dart
+++ lib/sora_flutter_sdk.dart
@@ -1,4 +1,4 @@
 export 'src/client.dart'
-    show SoraClient, SoraClientConfig, SoraRole, SoraVideoCodecType, SoraAudioCodecType;
+    show SoraClient, SoraClientConfig, SoraRole, SoraVideoCodecType, SoraAudioCodecType, SoraDataChannel;
 export 'src/video_track.dart' show SoraVideoTrack;
 export 'src/renderer.dart' show SoraRenderer;
