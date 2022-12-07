5692ac8d482391f799f6aa483e23c5d3fb406911
SUZUKI Tetsuya <szktty@shiguredo.jp>
2022-12-07 21:52:29 +0900

    WIP

--- lib/sora_flutter_sdk.dart
+++ lib/sora_flutter_sdk.dart
@@ -1,4 +1,13 @@
 export 'src/client.dart'
-    show SoraClient, SoraClientConfig, SoraRole, SoraVideoCodecType, SoraAudioCodecType, SoraDataChannel, SoraSimulcastRid, SoraSpotlightRid;
-export 'src/video_track.dart' show SoraVideoTrack;
+    show
+        SoraClient,
+        SoraClientConfig,
+        SoraRole,
+        SoraVideoCodecType,
+        SoraAudioCodecType,
+        SoraDataChannel,
+        SoraSimulcastRid,
+        SoraSpotlightRid;
+export 'src/device_list.dart' show DeviceList;
 export 'src/renderer.dart' show SoraRenderer;
+export 'src/video_track.dart' show SoraVideoTrack;
