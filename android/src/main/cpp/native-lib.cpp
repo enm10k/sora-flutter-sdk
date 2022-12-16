9735536e7dde3443d93da3b3224a7a5d05aa2c95
SUZUKI Tetsuya <szktty@shiguredo.jp>
2022-12-16 16:57:18 +0900

    WIP: カメラの切替 (Android)

--- android/src/main/cpp/native-lib.cpp
+++ android/src/main/cpp/native-lib.cpp
@@ -9,6 +9,7 @@
 
 #include "sora_client.h"
 #include "config_reader.h"
+#include "device_list.h"
 
 struct SoraClientWrapper {
   std::shared_ptr<sora_flutter_sdk::SoraClient> p;
@@ -144,4 +145,60 @@ Java_jp_shiguredo_sora_1flutter_1sdk_SoraFlutterSdkPlugin_sendDataChannel(JNIEnv
   webrtc::ScopedJavaLocalRef<jclass> resultcls(env, env->GetObjectClass(result));
   jmethodID successid = env->GetMethodID(resultcls.obj(), "success", "(Ljava/lang/Object;)V");
   env->CallVoidMethod(result, successid, boolobj.obj());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_jp_shiguredo_sora_1flutter_1sdk_SoraFlutterSdkPlugin_enumVideoCapturers(JNIEnv* env,
+                                         jobject /* this */, jobject call, jobject result) {
+  // alist = ArrayList();
+  webrtc::ScopedJavaLocalRef<jclass> alistcls = webrtc::GetClass(env, "java/lang/ArrayList");
+  jmethodID alist_ctorid = env->GetMethodID(alistcls.obj(), "<init>", "()V");
+  webrtc::ScopedJavaLocalRef<jobject> alistobj(env, env->NewObject(alistcls.obj(), alist_ctorid));
+  jmethodID alist_addid = env->GetMethodID(alistcls.obj(), "add", "(Ljava/lang/Object;)Z");
+
+  webrtc::ScopedJavaLocalRef<jclass> mapcls = webrtc::GetClass(env, "java/util/HashMap");
+  jmethodID map_ctorid = env->GetMethodID(mapcls.obj(), "<init>", "()V");
+  jmethodID map_putid = env->GetMethodID(mapcls.obj(), "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+  sora_flutter_sdk::DeviceList::EnumVideoCapturer(
+    [alistobj, alist_addid, mapcls, map_ctorid, map_putid, env]
+    (std::string device_name, std::string unique_name) {
+     // m = new HashMap();
+     // m.put("device", device_name);
+     // m.put("unique", unique_name);
+     // alist.add(m);
+    webrtc::ScopedJavaLocalRef<jobject> mapobj(env, env->NewObject(mapcls.obj(), map_ctorid));
+    env->CallObjectMethod(mapobj.obj(), map_putid, env->NewStringUTF("device"), env->NewStringUTF(device_name.c_str()));
+    env->CallObjectMethod(mapobj.obj(), map_putid, env->NewStringUTF("unique"), env->NewStringUTF(unique_name.c_str()));
+    env->CallObjectMethod(alistobj.obj(), alist_addid, mapobj.obj());
+  });
+
+  // result.success(alist);
+  webrtc::ScopedJavaLocalRef<jclass> resultcls(env, env->GetObjectClass(result));
+  jmethodID successid = env->GetMethodID(resultcls.obj(), "success", "(Ljava/lang/Object;)V");
+  env->CallVoidMethod(result, successid, alistobj.obj());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_jp_shiguredo_sora_1flutter_1sdk_SoraFlutterSdkPlugin_switchVideoDevice(JNIEnv* env,
+                                         jobject /* this */, jobject client, jobject call, jobject result) {
+   reinterpret_cast<SoraClientWrapper*>(client)->p->Connect();
+
+  sora::CameraDeviceCapturerConfig config;
+  // var json = call.argument<String[]>("config");
+  webrtc::ScopedJavaLocalRef<jclass> callcls(env, env->GetObjectClass(call));
+  jmethodID arg_id = env->GetMethodID(callcls.obj(), "argument", "(Ljava/lang/String;)Ljava/lang/Object;");
+  std::string json;
+  {
+    webrtc::ScopedJavaLocalRef<jstring> str(env, (jstring)env->CallObjectMethod(call, arg_id, env->NewStringUTF("config")));
+    const char* p = env->GetStringUTFChars(str.obj(), nullptr);
+    json = p;
+    env->ReleaseStringUTFChars(str.obj(), p);
+  }
+  config = sora_flutter_sdk::JsonToCameraDeviceCapturerConfig(json);
+  reinterpret_cast<SoraClientWrapper*>(client)->p->SwitchVideoDevice(config);
+
+   // result.success(null);
+   webrtc::ScopedJavaLocalRef<jclass> resultcls(env, env->GetObjectClass(result));
+   jmethodID successid = env->GetMethodID(resultcls.obj(), "success", "(Ljava/lang/Object;)V");
+   env->CallVoidMethod(result, successid, nullptr);
 }
