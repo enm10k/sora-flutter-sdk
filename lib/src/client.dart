a65ad863a9c9a5344dba09ceab7ee90f8b35b468
SUZUKI Tetsuya <szktty@shiguredo.jp>
2023-01-04 16:07:43 +0900

    カメラ切替時、切替完了まで await で待つように変更＆切替中の状態をプロパティで取得できるようにする

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -297,6 +297,8 @@ class SoraClient {
   /// クライアント ID
   int clientId = 0;
 
+  bool get switchingVideoDevice => _switchingVideoDevice;
+
   /// type: offer の受信時に呼ばれるコールバック
   void Function(String sdp)? onSetOffer;
 
@@ -331,6 +333,7 @@ class SoraClient {
   final Future<void> Function(SoraClient) _disposer;
   final Future<void> Function(SoraClient) _destructor;
   StreamSubscription<dynamic>? _eventSubscription;
+  bool _switchingVideoDevice = false;
 
   /// 本コンストラクタは内部実装で使われるので使わないでください。
   /// 本オブジェクトを生成するには [create] を使ってください。
@@ -460,18 +463,16 @@ class SoraClient {
     );
   }
 
-  bool _switchingDevice = false;
-
   Future<bool> switchVideoDevice({
     required String name,
     int? width,
     int? height,
     int? fps,
   }) async {
-    if (_switchingDevice) {
+    if (_switchingVideoDevice) {
       return false;
     } else {
-      _switchingDevice = true;
+      _switchingVideoDevice = true;
       await SoraFlutterSdkPlatform.instance.switchVideoDevice(
         client: this,
         name: name,
@@ -479,9 +480,9 @@ class SoraClient {
         height: height,
         fps: fps,
       );
-      Timer.periodic(const Duration(seconds: 2), (timer) {
-        _switchingDevice = false;
-      });
+
+      await Future.delayed(const Duration(milliseconds: 500));
+      _switchingVideoDevice = false;
       return true;
     }
   }
