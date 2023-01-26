fb41e0810e0aad559c9b2a719b170eeb3a494b02
miosakuma <mio.sakuma.gml@gmail.com>
2023-01-26 15:32:30 +0900

    バージョンを 2022.1.0-canary.11 にあげる

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: avoid_classes_with_only_static_members
 
 class Version {
-  static String sdkVersion = "2022.1.0-canary.10";
+  static String sdkVersion = "2022.1.0-canary.11";
 }
