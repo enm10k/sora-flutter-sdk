886e1c21a1a116b5c0f22f954ba824a73510877b
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-02-14 16:12:15 +0900

    [Linux] Lyra モデルファイルをアセットにコピーする

--- lib/src/lyra.dart
+++ lib/src/lyra.dart
@@ -8,7 +8,6 @@ import './sdk.dart';
 class Lyra {
   static const String _assetsDir = 'packages/sora_flutter_sdk/assets';
   static const String _modelDir = 'lyra/model_coeffs';
-  static const String _linuxModelDir = 'linux/_install/lyra/share/model_coeffs';
   static const String _sdkAppDocDir = 'sora_flutter_sdk';
 
   static const List<String> _modelFiles = [
@@ -40,19 +39,13 @@ class Lyra {
     await outDir.create(recursive: true);
     for (final file in _modelFiles) {
       late String asset;
-      if (Platform.isLinux) {
-        asset = '$_linuxModelDir/$file';
-      }else {
-        asset = '$_assetsDir/$_modelDir/$file';
-      }
+      asset = '$_assetsDir/$_modelDir/$file';
       final out = '${outDir.path}/$file';
       final outFile = File(out);
-      if (!(await outFile.exists())) {
-        final data = await rootBundle.load(asset);
-        await outFile.writeAsBytes(
-            data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes),
-            flush: true);
-      }
+      final data = await rootBundle.load(asset);
+      await outFile.writeAsBytes(
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes),
+          flush: true);
     }
 
     await SoraFlutterSdk.setLyraModelPath(outDir.path);
