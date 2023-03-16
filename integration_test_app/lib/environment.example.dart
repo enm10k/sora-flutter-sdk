c71bdb0b75843aca510f3c0a6677a3f23e8da108
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-03-16 15:02:56 +0900

    テスト用アプリを用意

---
+++ integration_test_app/lib/environment.example.dart
@@ -0,0 +1,17 @@
+// 接続設定
+
+// 以下の lint ルールを無視する
+// ignore_for_file: avoid_classes_with_only_static_members
+// ignore_for_file: unnecessary_nullable_for_final_variable_declarations
+
+class Environment {
+  static const String flutterVersion = '3.7.7';
+
+  static final List<Uri> urlCandidates = [
+    Uri.parse('wss://sora.example.com/signaling')
+  ];
+
+  static const String channelId = 'sora';
+
+  static const dynamic signalingMetadata = null;
+}
