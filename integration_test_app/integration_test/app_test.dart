5ecd35aae354dd5e2780d94cc2b706ea37b3e592
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-03-16 15:24:42 +0900

    テストの設定

--- integration_test_app/integration_test/app_test.dart
+++ integration_test_app/integration_test/app_test.dart
@@ -1,29 +1,15 @@
-import 'package:async/async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:integration_test_app/config.dart';
 import 'package:integration_test_app/main.dart' as app;
-import 'package:integration_test_app/test/event_controller.dart';
-import 'package:sora_flutter_sdk/sora_flutter_sdk.dart';
 
-// 実行方法
-// $ flutter test integration_test/app_test.dart -d <DEVICE_ID>
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('', () {
-    testWidgets('', (tester) async {
+    testWidgets('',
+        (tester) async {
       app.main();
       await tester.pumpAndSettle();
-
-      final config = createClientConfig(role: SoraRole.recvonly);
-      config.video = false;
-      config.audio = false;
-      final client = await SoraClient.create(config);
-      final controller = SoraClientEventController(client);
-      final queue = StreamQueue(controller.stream);
-      await controller.connect();
-      await controller.tryDispose();
     });
   });
 }
