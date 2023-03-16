0e5cc836e376eaa49553323e0743aabfd67b3d4f
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-03-16 17:16:05 +0900

    コールバックイベント管理の実装

--- integration_test_app/lib/test/event_controller.dart
+++ integration_test_app/lib/test/event_controller.dart
@@ -4,13 +4,12 @@ import './event_stream.dart';
 
 class SoraClientEventController {
   // TODO: state, timeout, 長時間配信用タイマー
-  SoraClientEventController(this.client, {int timeout = 5}) {
-    this.timeout = timeout;
+  SoraClientEventController(this.client) {
     stream = SoraClientEventStream(client);
   }
 
   final SoraClient client;
-  late final int timeout;
+
   late final SoraClientEventStream stream;
 
   bool get hasConnected => stream.hasOnNotify;
@@ -18,26 +17,15 @@ class SoraClientEventController {
   Future<void> connect({bool wait = true}) async {
     await client.connect();
     if (wait) {
-      print('wait: timeout $timeout');
-      var cont = true;
+      print('wait');
       await Future.doWhile(() async {
-        await Future.delayed(Duration(seconds: 1));
-        return cont && !hasConnected && !stream.hasOnDisconnect;
-      }).timeout(Duration(seconds: timeout), onTimeout: () {
-        cont = false;
+        return !hasConnected;
       });
-      print('end wait');
     }
   }
 
   Future<void> dispose() async {
     await client.dispose();
     stream.close();
-  }
-
-  Future<void> tryDispose() async {
-    if (!client.disposed) {
-      await dispose();
-    }
   }
 }
