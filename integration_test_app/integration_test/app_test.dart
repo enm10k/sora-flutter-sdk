76d0992da751e012062b22bd376081336b76c84d
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-03-17 17:29:05 +0900

    簡単な接続まで

--- integration_test_app/integration_test/app_test.dart
+++ integration_test_app/integration_test/app_test.dart
@@ -1,6 +1,10 @@
+import 'package:async/async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:integration_test_app/main.dart' as app;
+import 'package:integration_test_app/config.dart';
+import 'package:integration_test_app/test/event_controller.dart';
+import 'package:sora_flutter_sdk/sora_flutter_sdk.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +14,15 @@ void main() {
         (tester) async {
       app.main();
       await tester.pumpAndSettle();
+
+      final config = createClientConfig(role: SoraRole.recvonly);
+      //config.video = false;
+      //config.audio = false;
+      final client = await SoraClient.create(config);
+      final controller = SoraClientEventController(client);
+      final queue = StreamQueue(controller.stream);
+      await controller.connect();
+      await controller.dispose();
     });
   });
 }
