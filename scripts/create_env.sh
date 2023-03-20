e81bc91a11fad077bf2913ffb4ec6b8efc3e6500
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-03-20 16:45:33 +0900

    テスト用の enviroment.dart を生成するスクリプトを追加する

---
+++ scripts/create_env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# environment.dart を生成する
+# USAGE: create_env.sh OUTFILE URL_CANDIDATES CHANNEL_ID METADATA
+# EX) ./create_env.sh environment.dart url1,url2,url3 sora "{'signaling_key': '12345'}"
+
+FLUTTER_VERSION=$(echo $(flutter --version) | awk '/Flutter[[:space:]]+[[:digit:]]+/ {print $2}')
+
+OUTFILE=$1
+URLS=$2
+CHANNEL_ID=$3
+METADATA=$4
+
+# ファイルを空にする
+: > $OUTFILE
+
+echo "Generate $OUTFILE"
+cat << EOT >> $OUTFILE
+class Environment {
+  static const String flutterVersion = '$FLUTTER_VERSION';
+
+  static final List<Uri> urlCandidates = [
+EOT
+
+IFS=',' read -ra items <<< "$URLS"
+for item in "${items[@]}"; do
+    echo "    Uri.parse('$item')," >> $OUTFILE
+done
+
+cat << EOT >> $OUTFILE
+  ];
+
+  static const String channelId = '$CHANNEL_ID';
+
+  static const dynamic signalingMetadata = $METADATA;
+}
+EOT
