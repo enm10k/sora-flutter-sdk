121d2cb4a5325c6b29ccd97b992f7bf6aa78f330
enm10k <enm10k@gmail.com>
2023-09-13 00:44:28 +0900

    Flutter を 3.13.3 にアップデート

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
 import sora_flutter_sdk
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SoraFlutterSdkPlugin.register(with: registry.registrar(forPlugin: "SoraFlutterSdkPlugin"))
 }
