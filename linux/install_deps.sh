317468b588a2def6e570bb84aa7d745f34fa7bd9
enm10k <enm10k@gmail.com>
2023-11-04 13:13:17 +0900

    wip

--- linux/install_deps.sh
+++ linux/install_deps.sh
@@ -17,8 +17,8 @@ LYRA_VERSION=1.3.0
 mkdir -p $INSTALL_DIR
 
 if [ ! -e $INSTALL_DIR/webrtc ]; then
-  file=webrtc.ubuntu-20.04_x86_64-${WEBRTC_VERSION}.tar.gz
-  curl -Lo $INSTALL_DIR/$file https://github.com/shiguredo-webrtc-build/webrtc-build/releases/download/${WEBRTC_VERSION}/webrtc.ubuntu-20.04_x86_64.tar.gz
+  file=webrtc.ubuntu-20.04_x86_64.tar.gz
+  curl -Lo $INSTALL_DIR/$file https://github.com/shiguredo-webrtc-build/webrtc-build/releases/download/${WEBRTC_VERSION}/${file}
   tar -xf $INSTALL_DIR/$file -C $INSTALL_DIR
 fi
 
